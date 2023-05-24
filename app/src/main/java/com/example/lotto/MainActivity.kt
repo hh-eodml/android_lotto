173b4ada5d3223733a9497e8dad65bf3c7745503
hh-eodml <bmn3491@nate.com>
2023-05-24 13:21:09 +0900

    리포지터리 변경

---
+++ app/src/main/java/com/example/lotto/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.lotto
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.CountDownTimer
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val lotteryNumbers = arrayListOf(number1,number2,number3,number4,number5,number6)
+
+        val countDownTimer = object: CountDownTimer(3000, 100){
+
+            override fun onTick(millisUntilFinished: Long) {
+                lotteryNumbers.forEach {
+                    val randomSumber = (Math.random() * 45 + 1).toInt()
+                    it.text = "$randomSumber"
+                }
+            }
+
+            override fun onFinish() {
+            }
+
+        }
+
+        lotteryButton.setOnClickListener {
+            if (lotteryButton.isAnimating){
+                lotteryButton.cancelAnimation()
+                countDownTimer.cancel()
+            } else {
+                lotteryButton.playAnimation()
+                countDownTimer.start()
+            }
+
+        }
+    }
+}
